1c0813535ccb413a255db6a6f6f9a1faffa661ab
Jerry Applegarth <applegarth1630@gmail.com>
2021-11-10 07:54:09 -0800

    first final commit

--- app/src/main/java/com/fourapplecompose/powerballnumberpicker/MainActivity.kt
+++ app/src/main/java/com/fourapplecompose/powerballnumberpicker/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.fourapplecompose.powerballnumberpicker.ui.theme.DarkGreen
 import com.fourapplecompose.powerballnumberpicker.ui.theme.TestingTheme
+import com.fourapplecompose.powerballnumberpicker.ui.theme.newBackgroundColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestingTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colors.newBackgroundColor) {
                     PrintNumbers()
 
                 }
@@ -39,56 +40,46 @@ fun PrintNumbers() {
     Scaffold(
 
     ) {
-        TopAppBar { Text(text = "Power Ball Numbers",
-            fontSize = 20.sp,
+        TopAppBar {
+            Text(
+                text = "Power Ball Numbers",
+                fontSize = 20.sp,
 
-            maxLines = 1,
-            modifier = Modifier.padding(8.dp)
-
+                maxLines = 1,
+                modifier = Modifier.padding(8.dp)
             )
-
         }
-
     }
-
-
-
-    Column (modifier = Modifier
-        .fillMaxSize(),
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
 
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
-    ){
+    ) {
         Spacer(modifier = Modifier.height(16.dp))
         Text(
-            modifier = Modifier.background(Color.LightGray),
-            text = "Your Lucky Numbers",
 
+            text = "Your Lucky Numbers",
             fontStyle = FontStyle.Italic,
             fontFamily = FontFamily.Cursive,
             fontWeight = FontWeight.Bold,
-
             fontSize = 36.sp
-
         )
-        Text(modifier = Modifier.padding(16.dp),
+        Text(
+            modifier = Modifier.padding(16.dp),
             text = "Everytime you open this app, " +
                     "you will be shown your next lucky Power Ball number.\n" +
                     "You will not win every time but " +
                     "it's a new way to play-Let the app pick your numbers.",
-
             fontSize = 16.sp
         )
-
-        Text(text = "GoodLuck!!!",
+        Text(
+            text = "GoodLuck!!!",
             modifier = Modifier.padding(16.dp),
-            fontSize = 16.sp
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
         )
-
-
-
-
-
         val firstNumbers = (1..69).random()
         val secondNumbers = ((1..69) - firstNumbers).random()
         val thirdNumbers = ((1..69) - firstNumbers - secondNumbers).random()
@@ -96,43 +87,34 @@ fun PrintNumbers() {
                 - thirdNumbers).random()
         val fifthNumbers = ((1..69) - firstNumbers - secondNumbers
                 - thirdNumbers - forthNumbers).random()
-
         val powerballNumber = ((1..26) - firstNumbers - secondNumbers
                 - thirdNumbers - forthNumbers - fifthNumbers).random()
-
         val myLuckyNumber1: Int = firstNumbers
         val myLuckyNumber2: Int = secondNumbers
         val myLuckyNumber3: Int = thirdNumbers
         val myLuckyNumber4: Int = forthNumbers
         val myLuckyNumber5: Int = fifthNumbers
-
         val myPowerBall: Int = powerballNumber
 
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-
                 .background(Color.LightGray),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceAround,
-
             )
-
         {
-
             Text(
                 modifier = Modifier.padding(8.dp),
                 fontWeight = FontWeight.Bold,
                 text = "$myLuckyNumber1,",
                 fontSize = 24.sp,
-
                 )
             Text(
                 modifier = Modifier.padding(8.dp),
                 fontWeight = FontWeight.Bold,
                 text = "$myLuckyNumber2,",
                 fontSize = 24.sp,
-
                 )
             Text(
                 text = myLuckyNumber3.toString(),
@@ -140,35 +122,27 @@ fun PrintNumbers() {
                 fontSize = 24.sp,
                 modifier = Modifier.padding(8.dp),
             )
-
             Text(
                 text = myLuckyNumber4.toString(),
                 fontWeight = FontWeight.Bold,
                 fontSize = 24.sp,
                 modifier = Modifier.padding(8.dp),
             )
-
             Text(
                 text = myLuckyNumber5.toString(),
                 fontWeight = FontWeight.Bold,
                 fontSize = 24.sp,
                 modifier = Modifier.padding(8.dp),
-
                 )
             Text(
                 text = myPowerBall.toString(),
                 fontWeight = FontWeight.Bold,
                 fontSize = 24.sp,
-
                 color = Color.Red,
-
                 modifier = Modifier.padding(8.dp),
             )
         }
-
-
     }
-
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/fourapplecompose/powerballnumberpicker/ui/theme/Color.kt
+++ app/src/main/java/com/fourapplecompose/powerballnumberpicker/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.fourapplecompose.powerballnumberpicker.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFF85bb5c)
@@ -12,8 +14,60 @@ val Teal200 = Color(0xFF03DAC5)
 val LightGreen = Color(0xFFdcdec8)
 val DarkGreen = Color(0xFF558b2f)
 
+val RedOrange = Color(0xffffab91)
+val RedPink = Color(0xfff48fb1)
+val BabyBlue = Color(0xff81deea)
+val Violet = Color(0xffcf94da)
 
 
-//val newBackgroundColor: Color
-//    @Composable
-//    get() = if (lightColors) LightGreen else Purple500
+
+val LightYellow = Color(0xfffffde7)
+
+val LightGray = Color(0xfffcfcfc)
+val MediumGray = Color(0xff9c9c9c)
+val DarkGray = Color(0xff141414)
+
+val LowPriorityColor = Color(0xFF00c980)
+val MediumPriorityColor = Color(0xFFffc114)
+val HighPriorityColor = Color(0xFFff4646)
+val NonePriorityColor = MediumGray
+
+val Green200 = Color(0xffcfff95)
+val Green500 = Color(0xff9ccc65)
+val Green700 = Color(0xff6b9b37)
+val DarkGreen200 = Color(0xffaee571)
+val DarkGreen500 = Color(0xff7cb342)
+val DarkGreen700 = Color(0xff4b830d)
+
+
+val FullWhite = Color(0xffffffff)
+val FullBlack = Color(0xff000000)
+
+
+val Colors.newBackgroundColor: Color
+    @Composable
+    get() = if (isLight) LightGreen else DarkGreen700
+
+
+val Colors.taskItemTextColor: Color
+    @Composable
+    get() = if (isLight) FullBlack else DarkGreen500
+
+val Colors.taskItemBackgroundColor: Color
+    @Composable
+    get() = if (isLight) Color.White else DarkGray
+
+val Colors.fabBackgroundColor: Color
+    @Composable
+    get() = if (isLight) Green700 else DarkGreen700
+
+
+val Colors.topAppBarContentColor: Color
+    @Composable
+    get() = if (isLight) FullBlack else FullWhite
+
+val Colors.topAppBarBackgroundColor: Color
+    @Composable
+    get() = if (isLight) Green500 else DarkGreen500
+
+
